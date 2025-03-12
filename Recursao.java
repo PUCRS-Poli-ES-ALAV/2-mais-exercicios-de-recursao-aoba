57521b5acc7783d9a729387f3e1b545fbb1a1017
Paulo Carnovale <paulo.carnovale@edu.pucrs.br>
2025-03-12 20:10:30 -0300

    init initial

---
+++ Recursao.java
@@ -0,0 +1,25 @@
+public class Recursao {
+
+    /**
+     * Calcula o fatorial de um número n.
+     * 
+     * Modelagem:
+     * fatorial(0) = 1
+     * fatorial(n) = n * fatorial(n-1) para n > 0
+     * 
+     * @param n o número para calcular o fatorial
+     * @return o fatorial de n
+     */
+    public int fatorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * fatorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Recursao recursao = new Recursao();
+        System.out.println("Fatorial de 5: " + recursao.fatorial(5)); // Output: 120
+    }
+}
